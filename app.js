0c6d8ba31c7f3676c67250afe131e5cc76cbe4bf
Jensen Bouzi <jensen.bouzi@gmail.com>
2016-06-06 21:13:13 -0400

    create basic server in app.js

---
+++ app.js
@@ -0,0 +1,21 @@
+var express     = require('express'),
+    app         = express(),
+    bodyParser  = require('body-parser'),
+    router      = express.Router(),
+    port        = process.env.PORT || 3000;
+
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+router.use(function(req, res, next) {
+    // do logging
+    console.log('This is sample middleware for all routes');
+    next(); // make sure we go to the next routes and don't stop here
+});
+
+app.use('/', router);
+
+app.listen(port,function(){
+  console.log('Listening on Port: ', port);
+})
